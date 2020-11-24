8f7bb4c13eb8e5662f1333bdd922a9233c2e0aec
Ahmad <60389579+ahmadjafari86@users.noreply.github.com>
2020-11-25 00:24:10 +0330

    WebAccess
    Initialization of WebAccess layer in project

---
+++ WebAccess/WebScraping.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Runtime.InteropServices;
+
+namespace WebAccess
+{
+    class WebScraping
+    {
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
+
+        static void Main()
+        {
+            WebScraping er = new WebScraping();
+            // er.Scraper();
+            Console.WriteLine(er.IsConnectedToInternet());
+            string a = Console.ReadLine();
+        }
+        
+         public void Scraper()
+        {
+            HtmlAgilityPack.HtmlWeb webScraper = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = webScraper.Load("https://www.yellowpages.com/search?search_terms=Software&geo_location_terms=Sydney%2C+ND");
+            var headernames = doc.DocumentNode.SelectNodes("//a[@class='business-name']").ToList();
+            foreach (var item in headernames)
+            {
+                Console.WriteLine(item.InnerText);
+            }
+        }
+        
+        //Creating a function that uses the API function...
+        bool IsConnectedToInternet()
+        {
+
+            bool a;
+            int Desc;
+            a = InternetGetConnectedState(out Desc, 0);
+            return a;
+        }
+
+    }
+    
+}
